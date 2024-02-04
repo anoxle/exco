e733d88d22dd6ebb8e9fc804f353687d95d85ee8
anoxle <157937114+anoxle@users.noreply.github.com>
2024-02-04 11:03:29 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import discord
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+client = discord.Client()
+bot_id = 1200988881860972544  # Replace with your bot's ID
+
+@client.event
+async def on_ready():
+    global bot_online
+    bot_online = True
+
+@client.event
+async def on_disconnect():
+    global bot_online
+    bot_online = False
+
+@app.route('/')
+def index():
+    online_status = "Bot is online" if bot_online else "Bot is offline!"
+    return render_template('statusindex.html', status=online_status)
+
+if __name__ == '__main__':
+    client.run('your_bot_token')  # Replace with your bot's token
+    app.run(host='0.0.0.0', port=5000)  # Run Flask app
